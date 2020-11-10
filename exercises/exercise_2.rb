eeb5e282097b34d94baacf894e0f0c64a1dd540b
ozgeguner <ozgecaylioglu@gmail.com>
2020-11-10 20:24:33 +0000

    connected employee table

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -7,4 +7,5 @@ puts "----------"
 # Your code goes here ...
 @store1 = Store.find_by(id:1)
 @store2 = Store.find_by(id:2)
+@store4 = Store.find_by(id:4)
 @store1.update(name:"Mickey")
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,7 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Max", last_name: "Mad", hourly_rate: 30)
+@store1.employees.create(first_name: "Danny", last_name: "Luke", hourly_rate: 50)
